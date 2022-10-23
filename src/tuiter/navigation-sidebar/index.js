3f56b864c9e874312f0229e770f74aec63d6cba0
cole242000@gmail.com <cole242000@gmail.com>
2022-10-23 13:59:09 -0400

    resolved more warnings

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,43 +7,43 @@ const NavigationSidebar = (
 ) => {
  return (
    <div className="list-group">
-     <a className="list-group-item">Tuiter</a>
-     <a className={`list-group-item
+     <a href="/" className="list-group-item">Tuiter</a>
+     <a href="/home" className={`list-group-item
                     ${active === 'home'?'active':''}`}>
        <i className="bi bi-house-door-fill"></i>
               <p className="d-none d-lg-none d-xl-inline"> Home</p>
      </a>
-     <a className={`list-group-item
+     <a href="/explore" className={`list-group-item
                     ${active === 'explore'?'active':''}`}>
       <i class="bi bi-hash"></i>
              <p className="d-none d-lg-none d-xl-inline"> Explore</p>
      </a>
-     <a className={`list-group-item
+     <a href="/notifications" className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
       <i class="bi bi-bell-fill"></i>
              <p className="d-none d-lg-none d-xl-inline"> Notifications</p>
      </a>
-     <a className={`list-group-item
+     <a href="/messages" className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
      <i class="bi bi-envelope-fill"></i>
             <p className="d-none d-lg-none d-xl-inline"> Messages</p>
      </a>
-     <a className={`list-group-item
+     <a href="/bookmarks" className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
       <i class="bi bi-bookmark-fill"></i>
              <p className="d-none d-lg-none d-xl-inline"> Bookmarks</p>
      </a>
-     <a className={`list-group-item
+     <a href="/lists" className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
       <i class="bi bi-list-ul"></i>
              <p className="d-none d-lg-none d-xl-inline"> Lists</p>
      </a>
-     <a className={`list-group-item
+     <a href="/profile" className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
      <i class="bi bi-person-fill"></i>
             <p className="d-none d-lg-none d-xl-inline"> Profile</p>
      </a>
-     <a className={`list-group-item
+     <a href="/more" className={`list-group-item
                     ${active === 'more'?'active':''}`}>
      <i class="bi bi-three-dots"></i>
             <p className="d-none d-lg-none d-xl-inline"> More</p>
