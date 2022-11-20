e310e195f6f34d8768e7572b442e44fde5c10fa8
cole242000@gmail.com <cole242000@gmail.com>
2022-11-19 19:50:52 -0500

    adding link to server

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -12,7 +12,7 @@ import { configureStore }
   from '@reduxjs/toolkit';
 import {Provider} from "react-redux";
 const store = configureStore(
-  {reducer: {who: whoReducer, tuits: tuitsReducer}});
+  {reducer: {who: whoReducer, tuitsData: tuitsReducer}});
 
 function Tuiter() {
 var location = useLocation().pathname.split("/").pop()
@@ -21,24 +21,26 @@ location = "home";
 }
  return (
  <Provider store={store}>
+
    <div className="row mt-2">
      <div className="col-2 col-md-2 col-lg-1 col-xl-2">
        <NavigationSidebar active={location}/>
      </div>
      <div className="col-10 col-md-10 col-lg-7 col-xl-6"
           style={{"position": "relative"}}>
+ <Routes>
 
-       <Routes>
-              <Route path="*" element={<HomeComponent/>}/>
+              <Route path="/*" element={<HomeComponent/>}/>
               <Route path="/home" element={<HomeComponent/>}/>
               <Route path="/explore" element={<ExploreComponent/>}/>
-       </Routes>
 
+</Routes>
      </div>
      <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
        <WhoToFollowList/>
      </div>
    </div>
+
    </Provider>
  );
 }
--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -4,6 +4,9 @@ import { AiTwotoneMessage } from 'react-icons/ai';
 import { FaRetweet } from 'react-icons/fa';
 import { AiFillHeart } from 'react-icons/ai';
 import { BsShareFill } from 'react-icons/bs';
+import { FaThumbsDown } from 'react-icons/fa';
+import {updateTuitThunk} from "../../services/tuits-thunks";
+import {useDispatch, useSelector} from "react-redux";
 
 const TuitStats = ({
                     tuit = {
@@ -14,6 +17,7 @@ const TuitStats = ({
                          "image": "tesla.png"
                          }
                      }) => {
+ const dispatch = useDispatch();
  return(
    <div className="row">
    <div className="col-2">
@@ -24,13 +28,17 @@ const TuitStats = ({
       <FaRetweet/>
       {' ' + tuit.retuits}
       </div>
-      <div className="col-2">
-         {tuit.liked? <AiFillHeart className="text-danger"/> : <AiFillHeart/>}
-         {' ' + tuit.likes}
+      <div className="col-4">
+           Likes: {tuit.likes}
+           <AiFillHeart onClick={() => dispatch(updateTuitThunk({...tuit, liked: true, likes: tuit.likes + 1}))} className={tuit.liked? "text-danger" : ""}/>
          </div>
+         <div className="col-4">
+           Dislikes: {tuit.dislikes}
+           <FaThumbsDown onClick={() => dispatch(updateTuitThunk({...tuit, disliked: true, dislikes: tuit.dislikes + 1}))} className={tuit.disliked? "text-danger" : ""}/>
+           </div>
          <div className="col-2">
             <BsShareFill/>
-            </div>
+          </div>
    </div>
  );
 };
